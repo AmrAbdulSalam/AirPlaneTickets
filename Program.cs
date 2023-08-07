57363cb6ef631653b0d7e2a9f50f4dadad7dded9
AmrAbdulSalam <amrsalam@hotmail.com>
2023-08-07 23:19:05 +0300

    Init project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AirPlanceTickets
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("AirPlane tickets");
+        }
+    }
+}
