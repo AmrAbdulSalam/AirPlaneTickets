67b1bdf941169144d921a72357fd711cd514bf4c
AmrAbdulSalam <amrsalam@hotmail.com>
2023-08-08 11:07:47 +0300

    Adding FlightDTO class to hold data for flights

---
+++ FlightDTO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AirportTickets.Flight
+{
+    public class FlightDTO : ILoggable
+    {
+        public enum ClassType
+        {
+            EconEconomy,
+            Business,
+            FirstClass,
+        }
+
+        public string Departure { get; set; }
+        public string Destination { get; set; }
+        public decimal Price { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public string ArrivalAirport { get; set; }
+        public string DepartureAirport { get; set; }
+        public ClassType ServiceClass { get; set; }
+
+        public FlightDTO()
+        {
+
+        }
+
+        public override string ToString() => $"From : {Departure} -> To : {Destination}.";
+
+        public void Log() => Console.WriteLine($"{DepartureAirport} -> {ArrivalAirport} At : {DepartureDate}");
+    }
+}
+
---
+++ ILoggable.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace AirportTickets
+{
+    public interface ILoggable
+    {
+        void Log();
+    }
+}
+
