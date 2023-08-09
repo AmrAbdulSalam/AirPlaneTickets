f96ffef443981381b43c3431be66a5600b46eb81
AmrAbdulSalam <amrsalam@hotmail.com>
2023-08-09 13:51:54 +0300

    Building a booking class to hold both name and flight booked information

---
+++ Booking.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace AirportTickets.Booking
+{
+    public class BookingDTO
+    {
+        List<ReservationDTO> _booked = new();
+
+        public BookingDTO()
+        {
+
+        }
+
+        public void AddBooking(ReservationDTO reservation)
+        {
+            _booked.Add(reservation);
+        }
+
+        public List<ReservationDTO> RetreiveBookings()
+        {
+            return _booked;
+        }
+    }
+}
---
+++ ReservationDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using AirportTickets.Flight;
+
+namespace AirportTickets
+{
+    public class ReservationDTO
+    {
+        public string Name { get; private set; }
+        public FlightDTO Flight { get; private set; }
+
+        public ReservationDTO(string name, FlightDTO flight)
+        {
+            Name = name;
+            Flight = flight;
+        }
+    }
+}
