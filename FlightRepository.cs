48d1456fbcea1d8f90c686914b268a65cfacf51e
AmrAbdulSalam <amrsalam@hotmail.com>
2023-08-09 12:15:51 +0300

    Adding methods to search for flights

--- FlightRepository.cs
+++ FlightRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using AirportTickets.FileInfo;
 using AirportTickets.Flight;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace AirportTickets.Repository
 {
@@ -14,5 +15,52 @@ namespace AirportTickets.Repository
         }
 
         public List<FlightDTO> FlightsInfo() => _flights;
+
+        public List<FlightDTO> SearchByDeparture(string departureCountry)
+        {
+            return _flights.Where(flight => flight.Departure.Contains(departureCountry))
+                .OrderBy(flight => flight.DepartureDate).ToList();
+        }
+
+        public List<FlightDTO> SearchByDestination(string destinationCountry)
+        {
+            return _flights.Where(flight => flight.Destination.Contains(destinationCountry))
+                .OrderBy(flight => flight.DepartureDate).ToList();
+        }
+
+        public List<FlightDTO> SearchByDepartureDate(DateTime date)
+        {
+            return _flights.Where(flight => flight.DepartureDate.Date == date.Date)
+                .OrderBy(flight => flight.DepartureDate).ToList();
+        }
+
+        public List<FlightDTO> SearchByPrice(decimal price, FlightDTO.ClassType type)
+        {
+            return _flights.Where(flight => flight.ID == flight.GetPrices(price, type))
+                .OrderBy(flight => flight.DepartureDate).ToList();
+        }
+
+        public List<FlightDTO> SearchByDepartureAirport(string departureAirport)
+        {
+            return _flights.Where(flight => flight.DepartureAirport == departureAirport)
+                .OrderBy(flight => flight.DepartureDate).ToList();
+        }
+
+        public List<FlightDTO> SearchByArrivalAirport(string arrivalAirport)
+        {
+            return _flights.Where(flight => flight.ArrivalAirport == arrivalAirport)
+               .OrderBy(flight => flight.DepartureDate).ToList();
+        }
+
+        public List<FlightDTO> SeachByClassType(FlightDTO.ClassType type)
+        {
+            return _flights.Where(flight => flight.ID == flight.FindClassType(type))
+               .OrderBy(flight => flight.DepartureDate).ToList();
+        }
+
+        public FlightDTO SearchForFlight(string id)
+        {
+            return _flights.Single(flight => flight.ID == id);
+        }
     }
 }
