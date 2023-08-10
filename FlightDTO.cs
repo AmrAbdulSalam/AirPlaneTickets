b9dc6ef121294992010b5ca6582734ff3658c345
AmrAbdulSalam <amrsalam@hotmail.com>
2023-08-10 10:21:24 +0300

    Validation attributes for each property

--- FlightDTO.cs
+++ FlightDTO.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using AirportTickets.FutureDate;
 
 namespace AirportTickets.Flight
 {
@@ -13,12 +16,35 @@ namespace AirportTickets.Flight
 
         private Dictionary<ClassType, decimal> _prices = new();
 
+        [Required(ErrorMessage = "{0} is required")]
+        [DataType(DataType.Text)]
         public string ID { get; init; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [DataType(DataType.Text)]
         public string Departure { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [DataType(DataType.Text)]
         public string Destination { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [DataType(DataType.DateTime)]
+        [FutureDate(ErrorMessage = "{0} Must be a futrue date")]
         public DateTime DepartureDate { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [DataType(DataType.Text)]
+        [StringLength(3, MinimumLength = 3, ErrorMessage = "Arival Airport should only have 3 character according to  'IATA code'")]
         public string ArrivalAirport { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [DataType(DataType.Text)]
+        [StringLength(3, MinimumLength = 3, ErrorMessage = "Departure Airport should only have 3 character according to  'IATA code'")]
         public string DepartureAirport { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [EnumDataType(typeof(ClassType), ErrorMessage = "Invalid Class")]
         public ClassType ServiceClass { get; set; }
 
         public FlightDTO()
@@ -35,7 +61,7 @@ namespace AirportTickets.Flight
         {
             foreach (var item in _prices)
             {
-                if(item.Key == classType && item.Value != null)
+                if (item.Key == classType && item.Value != null)
                 {
                     return ID;
                 }
---
+++ FutureDateAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AirportTickets.FutureDate
+{
+    public class FutureDateAttribute : ValidationAttribute
+    {
+        public override bool IsValid(Object value)
+        {
+            if (value is DateTime date)
+            {
+                return date >= DateTime.Today;
+            }
+            return false;
+        }
+    }
+}
